41dede328391af8f160b437e936e368b4930f2d9
Amlaan Bhoi <amlaanb@gmail.com>
2018-10-06 14:38:38 -0500

    Initial Commit

---
+++ quicklook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quicklook
+//
+//  Created by CH-ABHOI-MBP on 10/6/18.
+//  Copyright © 2018 Octabit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
